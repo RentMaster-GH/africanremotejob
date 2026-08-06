59a0c216a5c2047ccfd6714574de17ad05cbea50
RentMaster-GH <papastickle@googlemail.com>
2026-08-06 12:41:32 +0000

    Force dynamic rendering on data-fetching pages to prevent static generation OOM crash

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
   productionBrowserSourceMaps: false,
   experimental: {
     webpackMemoryOptimizations: true,
-    cpus: 1,
   },
 }
 
-export default nextConfig
+module.exports = nextConfig
